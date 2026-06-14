a87976b1cc066486985c78ec8511edddd3242e10
nielday <phongvipotaku@gmail.com>
2026-06-14 16:28:30 +0700

    Add vercel.json fallback and Axios 401 interceptor

--- src/utils/axios.ts
+++ src/utils/axios.ts
@@ -20,4 +20,19 @@ api.interceptors.request.use((config) => {
   return Promise.reject(error)
 })
 
+api.interceptors.response.use(
+  (response) => {
+    return response
+  },
+  (error) => {
+    if (error.response && (error.response.status === 401 || error.response.status === 403)) {
+      const authStore = useAuthStore()
+      authStore.logout()
+      // Use window.location to force a hard redirect to clear state
+      window.location.href = '/login'
+    }
+    return Promise.reject(error)
+  }
+)
+
 export default api
